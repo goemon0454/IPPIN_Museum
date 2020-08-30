fa632e797121b49601409ec9b55b554c5d9f6782
unknown <turtle.goepro.0454@gmail.com>
2020-08-30 18:55:02 +0900

    restart

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,6 @@ DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
