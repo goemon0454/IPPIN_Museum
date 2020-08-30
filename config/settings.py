a2cd27ba1e4057a9a874a81c124b71805eddf8fb
unknown <turtle.goepro.0454@gmail.com>
2020-08-30 16:52:00 +0900

    run collectstatic

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 IMAGE_ROOT = os.path.join(BASE_DIR, "images")
 IMAGE_URL = "/images/"
 
