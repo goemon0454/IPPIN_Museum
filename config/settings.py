a45a33debc6a885a48b628203f489c7a9bfd995d
unknown <turtle.goepro.0454@gmail.com>
2020-08-30 17:50:40 +0900

    STATIC_ROOT return

--- config/settings.py
+++ config/settings.py
@@ -143,7 +143,7 @@ DATABASES['default'].update(db_from_env)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
